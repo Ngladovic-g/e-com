a665e0789e65e2b673c034faa07f7eeaa5d1c6c0
Ngladovic <gladovicnenad2@gmail.com>
2026-03-17 20:43:22 +0100

    nglad2user added

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -49,4 +49,16 @@ export class HomePage {
         }
     }
 
+    // this was added by user nglad2 branch
+    async nglad2Userfunction():Promise<RegistrationPage>{
+        try {
+            await this.registerLink.click();
+            return new RegistrationPage(this.page);
+        }
+        catch (error) {
+            console.log(`Expception while clicking on Registration link: ${error}`)
+            throw error;
+        }
+    }
+
 }
